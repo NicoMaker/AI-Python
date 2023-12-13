dc75a49eb6f21bce02dad0b49d0d08d5fa9c7fd8
Nico Maker <nicola.marano02@gmail.com>
2023-12-13 15:21:21 +0100

    4 lezione v1

---
+++ 4 lezione 13_12_2023/Codice/Macchine Learning/IRIS/IRIStoexcel.py
@@ -0,0 +1,13 @@
+# python -m pip install pandas
+# python -m pip install openpyxl
+
+import pandas as pd
+import openpyxl
+
+data =  pd.read_csv("D:\\Corso ITS IIOT\\2 anno\\AI Python\\AI-Python\\4 lezione 13_12_2023\\Codice\\Macchine Learning\\Dati per es\\csv2.CSV")
+print(data)  #stamppa dati
+
+data.info()
+for row in data:
+    print(row)
+data.to_excel("D:\\Corso ITS IIOT\\2 anno\\AI Python\\AI-Python\\4 lezione 13_12_2023\\Codice\\Macchine Learning\\IRIS\\Result IRIS\\outputfileexcel.xlsx" , index=False)    
---
+++ 4 lezione 13_12_2023/Codice/Macchine Learning/IRIS/IRIStoword.py
@@ -0,0 +1,6 @@
+
+# python -m pip install docx2txt
+
+import docx2txt
+mytext = docx2txt.process("D:\\Corso ITS IIOT\\2 anno\AI Python\AI-Python\\4 lezione 13_12_2023\Codice\Macchine Learning\\Dati per es\\doc1.docx")
+print(mytext)
